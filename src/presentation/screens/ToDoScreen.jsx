4b01b0eab1f0cf1f817ddf9a181becb069e5b81d
tuikobt <leminhphuong15032005@gmail.com>
2025-09-23 23:54:45 +0700

    fix: safeviewarea in Android

--- src/presentation/screens/ToDoScreen.jsx
+++ src/presentation/screens/ToDoScreen.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { View, StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import { View, StyleSheet, StatusBar } from "react-native";
 import ToDoList from "../components/ToDoList.jsx";
 import { useTodos } from "../hooks/useTodos.js";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function ToDoScreen() {
   const { todos, loading, error, addTodo, toggleTodo } = useTodos();
 
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <ToDoList
-        todos={todos}
-        onAddTodo={addTodo}
-        onToggleTodo={toggleTodo}
-        loading={loading}
-        error={error}
-      />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <ToDoList
+          todos={todos}
+          onAddTodo={addTodo}
+          onToggleTodo={toggleTodo}
+          loading={loading}
+          error={error}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
